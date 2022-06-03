245408b4d25104f8d6e779f79ac3fcf42ee760fe
Calan Keller <Calzone407@gmail.com>
2022-06-03 11:07:58 -0500

    Health bar + movement

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,69 +1,140 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour
 {
-  public Rigidbody2D rb;
-  public float speed;
-  //[Header("Ground Check Variables")]
-  public bool isGrounded;//Boolean. if is on ground sets to true
-  public float checkRadius;//decides how large the radius of the ground posiiton
-  public LayerMask whatIsGround;//references the ground layer
-  public Transform groundPos;//This is the empty game object attached to the player, which checks for Ground
+    //Horizontal Movement Variables                                                                                                       //misc
+    private float moveInput;                                                                                                                    private Rigidbody2D rb;// Reference to the rigidbody2d
+    public float speed;                                                                                                                              private Collider2D collider;
 
-  //[Header("Jumping Variables")]
-  private float jumpTimeCounter;//What does this variable do lol
-  private bool doubleJump;//False by default, is true if isGrounded is false and isJumping is false
-  private bool isJumping;//False by default, sets to true if Z is input
-  public float jumpForce;//how much force goes in the jump
-  public float jumpTime;//How much time you are in the air
-  void Update()
-  {
-    Jump();
-    Walk();
-  }
-  public void Walk()
-  {
+    //Vertical Movement Variables
+    public LayerMask Ground;//this is the ground layer in unity
+    public Transform groundCheck;//the groundCheck object is a child of player
+    public float checkRadius;//the size of the check radius
+    public float jumpSpeed;//speed of the jump
+    public float jumpTime;//time you are in the air
+    private bool isJumping;
+    public float jumpForce;//total jump force
+    public float fallSpeed;
+    private float jumpTimeCounter;
+    //public bool isGrounded;//the boolean to check if you are on thye ground
+    private bool canJump;
+    private float pressedJump;
+    public float pressedJumpTime;
+    public float groundRememberTime;
+    private float groundRemember;
+
+    //Enemy Variables
+    public Transform enemyCheck;
+    public LayerMask Enemy;
+    public float enemyJumpSmall;
+    public float enemyJumpBig;
+    public float enemyCheckRadius;
+    private bool enemyChecker;
+    private bool isOnEnemy;
+
+    //animation
+    public Animator anim;
+    public hungry hungry;
+    public int maxHungry = 100;
+    public int currentHungry;
+    bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>();
+      collider = GetComponent<Collider2D>();
+      currentHungry = maxHungry;
+      hungry.setMaxHealth(maxHungry);
+    //  anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      Move();//call the move function
 
 
-    float moveInput = Input.GetAxisRaw("Horizontal");//checks for input on the arrow keys
-    rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);//moves left or right based on input
+    }
+    void Update()
+    {
+      Jump();//Call the jump function
+    }
 
-
-    if(moveInput < 0)
+    void Move()
+    {
+      moveInput = Input.GetAxisRaw("Horizontal");
+      if(moveInput == 1 || moveInput == -1)
       {
-        transform.eulerAngles = new Vector3(0, 0, 0);//if Player input is left, makes them face left
-      }
+        isMoving = true;
 
-    else if(moveInput > 0)
+      }
+      else
       {
-        transform.eulerAngles = new Vector3(0, 180, 0);//if player input is right makes them face right
+       isMoving = false;
       }
-  }
-  public void Jump()
+      if(isMoving)
+      {
+    //    anim.SetBool("isRunning", true);
+      }
+      else
+      {
+    //    anim.SetBool("isRunning", false);
+      }
+      if(moveInput < 0)
+        {
+          transform.eulerAngles = new Vector3(0, 180, 0);//if Player input is left, makes them face left
+        }
+
+      else if(moveInput > 0)
+        {
+          transform.eulerAngles = new Vector3(0, 0, 0);//if player input is right makes them face right
+        }
+
+      //transform.Translate(new Vector2(moveInput, 0) * speed * Time.deltaTime);//Move left and right
+      rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+
+  void Jump()
   {
-    isGrounded = Physics2D.OverlapCircle(groundPos.position, checkRadius, whatIsGround);//checks for ground
-
-    if(isGrounded == true && Input.GetKeyDown(KeyCode.Z))//Checks to see if is inputing Z, and is geounded, before allowing a jump
-    {
-      jumpTimeCounter = jumpTime;//times the jump
-      rb.velocity = Vector2.up * jumpForce;//This is the actual jump
-    }
-
-    if(isGrounded == true)//Checks to see if isGrounded
-    {
-      //isJumping = false;//Sets jumping to false
-      doubleJump = false;//Double jump is false
-    }
-
-    if(isGrounded == false && doubleJump == false && Input.GetKeyDown(KeyCode.Z))//If you aren't grounded, and are inputing Z, and also havent double jumped yet
-    {
-      doubleJump = true;//sets double jump to true, so you can't jump again until grounded
-      jumpTimeCounter = jumpTime;//times the jump
-      rb.velocity = Vector2.up * jumpForce;//This is the jump
-    }
-
+  bool  isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, Ground);
+  groundRemember -= Time.deltaTime;
+  if(isGrounded)
+  {
+    groundRemember = groundRememberTime;
   }
+pressedJump -= Time.deltaTime;
+if(Input.GetKeyDown(KeyCode.Space))
+{
+  pressedJump = pressedJumpTime;
 }
+
+    if((groundRemember > 0) && (pressedJump > 0))
+    {
+      pressedJump = 0;
+      groundRemember = 0;
+      rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    if(Input.GetKeyUp(KeyCode.Space))
+      {
+      if(rb.velocity.y > 0)
+        {
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * fallSpeed);
+        }
+      }
+    }
+
+    public void HungryBar()
+    {
+      if(true)
+      {
+      currentHungry -= 1;
+      hungry.SetHealth(currentHungry);
+    }
+    }
+  }
